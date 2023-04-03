7662bee398b6adb82f4785b96ca20259f6bb2891
ToDuyVuong <comc5288@gmail.com>
2023-04-03 16:40:22 +0700

    customer

---
+++ src/main/java/vn/nhom08/smarthome/entity/CustomerEntity.java
@@ -0,0 +1,21 @@
+package vn.nhom08.smarthome.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerEntity {
+    @Id
+    private Integer customerId;
+
+    private  String customerFullName;
+}
