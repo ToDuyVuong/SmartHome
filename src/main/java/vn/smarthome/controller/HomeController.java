69159341d4622a97abc10bd1a0cf6e24aa6cd369
venhha123 <venh.ha@gmail.com>
2023-04-14 22:30:48 +0700

    base

--- src/main/java/vn/smarthome/controller/HomeController.java
+++ src/main/java/vn/smarthome/controller/HomeController.java
@@ -2,13 +2,14 @@ package vn.smarthome.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
-    public String index(Model model) {
-        model.addAttribute("message", "Hello World");
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, World!");
         return "index";
     }
 }
