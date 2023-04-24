05d2b6693f538212279e20385ee92e6bb8e5633d
DucTrungDo <ducdt2192@gmail.com>
2023-04-24 21:41:58 +0700

    Fix bug

---
+++ src/main/java/vn/smarthome/model/CategoryModel.java
@@ -0,0 +1,13 @@
+package vn.smarthome.model;
+
+import vn.smarthome.entity.Product;
+
+import java.util.List;
+
+public class CategoryModel {
+    private int categoryId;
+    private String name;
+    private String description;
+    private List<Product> products;
+
+}
