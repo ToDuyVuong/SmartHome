cc6427975cba66b4f44bdcc1240fe7f7feb39211
ToDuyVuong <comc5288@gmail.com>
2023-04-25 10:35:04 +0700

    vuong 11

--- src/main/java/vn/smarthome/SmarthomeApplication.java
+++ src/main/java/vn/smarthome/SmarthomeApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmarthomeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SmarthomeApplication.class, args);
+		System.out.println(" 🚀 Server ready at http://localhost:8080");
+
 	}
 
 }
--- src/main/java/vn/smarthome/controller/admin/AdminController.java
+++ src/main/java/vn/smarthome/controller/admin/AdminController.java
@@ -1,4 +1,4 @@
-package vn.smarthome.admin;
+package vn.smarthome.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
