2e98ac615a317ba7bc2861f01d684cd61cfdfc56
DucTrungDo <ducdt2192@gmail.com>
2023-04-25 20:23:36 +0700

    fix

--- src/main/java/vn/smarthome/controller/admin/AdminController.java
+++ src/main/java/vn/smarthome/controller/admin/AdminController.java
@@ -1,31 +1,48 @@
 package vn.smarthome.controller.admin;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import vn.smarthome.entity.CartItem;
+import vn.smarthome.entity.Category;
+import vn.smarthome.entity.Customer;
+import vn.smarthome.entity.Product;
+import vn.smarthome.model.CategoryModel;
+import vn.smarthome.model.CustomerModel;
+import vn.smarthome.service.ICategoryService;
+import vn.smarthome.service.IProductService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("admin")
 public class AdminController {
-    //    @Autowired
-//    IProductService productService;
-//    @RequestMapping(value = {"/admin", "admin"}, method = RequestMethod.GET)
-//    public String StartHonme(/*ModelMap modelMap*/)
-//    {
-//
-//
-////        List<ProductEntity> productEntityList = new ArrayList<>();
-////
-////        productEntityList = productService.findAll();
-////
-////        for (ProductEntity productEntity : productEntityList) {
-////            System.out.println(productEntity.getProductName());
-////        }
-////
-////        modelMap.addAttribute("productList", productEntityList);
-//
-//
-//
-//        return "admin/addcategory";
-//    }
+    @Autowired
+    ICategoryService categoryService;
+    @RequestMapping("/listCategory")
+    public ModelAndView ListCategory(ModelMap model, HttpServletRequest request) {
+        List<Category> categories = categoryService.findAll();
 
+        if (!categories.isEmpty()) {
+            CategoryModel categoryModel = new CategoryModel();
+            BeanUtils.copyProperties(categories, categoryModel);
+            model.addAttribute("listP", categories);
+        }
+        return new ModelAndView("admin/listcategory", model);
+    }
+    @RequestMapping("/deleteCategory/{categoryId}")
+    public ModelAndView RemoveProductToCart(ModelMap model, @PathVariable("categoryId") Integer categoryId) {
+        Optional<Category> opt = categoryService.findById(categoryId);
+        if (opt.isPresent()) {
+            categoryService.deleteCategoryByCategoryId(categoryId);
+        }
+        return new ModelAndView("redirect:/admin/listCategory", model);
+    }
 }
